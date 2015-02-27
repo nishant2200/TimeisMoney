f306127052701112af23aa1696710c5ca994c129
nishant2200 <nishant2200@gmail.com>
2015-02-27 08:09:33 -0800

    here we go

---
+++ TimeIsMoney/Controllers/FireController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeIsMoney.Controllers
+{
+    public class FireController : Controller
+    {
+        //
+        // GET: /Fire/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Fire/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + ID);
+        }
+	}
+}
