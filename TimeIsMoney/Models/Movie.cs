3e1ff571034cdfc2471abd6c9c3220d73def023b
nishant2200 <nishant2200@gmail.com>
2015-03-05 13:45:03 -0800

    1

--- TimeIsMoney/Models/Movie.cs
+++ TimeIsMoney/Models/Movie.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace TimeIsMoney.Models
@@ -8,6 +8,10 @@ namespace TimeIsMoney.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name="Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
